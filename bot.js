59a01c67aaf18f9dd69a7f5e6f1644b4e2f74a2f
Mikkel Jakobsen <jakobsen.mikkel@gmail.com>
2022-03-16 20:14:24 +0100

    Initial commit

---
+++ bot.js
@@ -0,0 +1,52 @@
+const { default: axios } = require("axios");
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+const prefix = "!";
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const API_KEY = process.env.API_KEY || '';
+
+let karmaStore = config.users.reduce((obj, item) => {
+    return {
+      ...obj,
+      [item]: 0,
+    };
+  }, {});
+
+const constructGiphyUrl = ({ url, apiKey, searchTerm }) =>
+  `${url}?api_key=${apiKey}&q=${searchTerm}`;
+
+client.on("messageCreate", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "karma") {
+    const messageData = [];
+    message.mentions.users.map((user) => {
+      const karma = karmaStore[user.username]++;
+      messageData.push(`Yay! ${user} har nu: ${karma} point`);
+    });
+
+    axios.get(constructGiphyUrl({...config.giphy, apiKey: API_KEY}))
+    .then(function (response) {
+      const { data: { data } } = response;
+
+      if (data.length) {
+        const gif = data[Math.floor(Math.random() * data.length)];
+        messageData.push(gif.url);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function (){
+      message.reply(messageData.join("\n"));
+    });
+  }
+
+});
+
+client.login(BOT_TOKEN);
